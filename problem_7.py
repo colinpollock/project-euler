e3b8203314b65b9498351fa9f4c7115df4b1cc31
Colin <pollockcolin@gmail.com>
2017-08-01 23:21:29 -0700

    committing whatever is in this dir since computer is making croaking sounds

---
+++ problem_7.py
@@ -0,0 +1,20 @@
+NOT DONE
+
+"""Problem 7 - 10001st prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+http://projecteuler.net/problem=7
+"""
+
+def nth_prime(n):
+    
+
+
+if __name__ == '__main__':
+    found = nth_prime(6)
+    print found
+    assert found == 13
---
+++ problem_9.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+NOT DONE
+"""Problem 9 - Special Pythagorean triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
+    a**2 + b**2 = c**2
+
+For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+
+http://projecteuler.net/problem=9
+"""
+
+c**2 = a**2 + b**2
+c == sqrt(a**2 + b**2)
+c = 1000 - a - b
+
+sqrt(a**2 + b**2) == 1000 - a - b
